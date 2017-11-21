f77d932832e5fec9567e43e5ccd7191a8e839d8f
J Cole Morrison <j.cole.morrison@gmail.com>
2017-11-20 21:53:53 -0800

    init question flow

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const cmd = require('commander')
+const init = require('./init')
 
 cmd.version('1.0.0')
   .description('a tool and foundation for building production cloudformation templates')
@@ -6,23 +7,49 @@ cmd.version('1.0.0')
 cmd
   .command('init')
   .description('initialize a new CloudFoundation project')
-  .action(() => {
-    console.log('yep')
-  })
+  .action(init)
 
 cmd
   .command('build')
-  .description('build down all CloudFormation templates\n')
+  .description('build down all CloudFormation templates')
   .action(() => {
     console.log('building')
   })
 
+cmd
+  .command('deploy <stackname>')
+  .description('deploy <stackname> template')
+  .action((env, options) => {
+    console.log(env)
+  })
+
+cmd
+  .command('update <stackname>')
+  .description('update <stackname> template')
+  .action(() => {
+    console.log('building')
+  })
+
+cmd
+  .command('create <stackname>')
+  .description('create a new stack named <stackname>')
+  .action(() => {
+    console.log('creating')
+  })
+
+cmd
+  .command('add <filepath>')
+  .description('add an AWS resource to the specified file')
+  .option("-r, --resource [resource]", "exact name of the CFN resource to add to the file i.e. AWS::EC2::Instance")
+  .action((env, options) => {
+    console.log(env, options)
+  })
+
 cmd.parse(process.argv)
 
-const validArgs = ['init', 'build']
-console.log(cmd.args,  cmd._execs)
+const validArgs = ['init', 'build', 'add']
+// console.log(cmd.args,  cmd._execs)
 
-
-if (cmd.args.length < 1 || cmd.args.length >= 1 && validArgs.indexOf(cmd.args[0]) === -1) {
+if (cmd.args.length < 1 || validArgs.indexOf(cmd.args[cmd.args.length - 1]._name) === -1) {
   cmd.help()
 }
--- src/init/index.js
+++ src/init/index.js
@@ -1,1 +1,49 @@
 const inq = require('inquirer')
+const chk = require('chalk')
+
+module.exports = async function init (env, options) {
+  console.log('executing')
+  let answers, extras
+  try {
+    answers = await inq.prompt([
+      {
+        type: "input",
+        name: "stackname",
+        message: "What is the name of your stack?",
+      },
+      {
+        type: "confirm",
+        name: "vpc",
+        message: "Would you like a production VPC?",
+        default: true,
+      }
+    ])
+  } catch (err) {
+    console.log(err)
+    return err
+  }
+
+  if (answers && answers.vpc) {
+    try {
+      extras = await inq.prompt([
+        {
+          type: "confirm",
+          name: "bastion",
+          message: "Would you like a bastion server for your VPC?",
+          default: true,
+        },
+        {
+          type: "confirm",
+          name: "rds",
+          message: "Would you like an encrypted, multi-AZ RDS Aurora Database?",
+          default: true,
+        }
+      ])
+    } catch (err) {
+      console.log(err)
+      return err
+    }
+  }
+
+  console.log(answers, extras)
+}
