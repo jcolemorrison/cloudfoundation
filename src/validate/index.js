41d3d4acc5d79af100c340e71dc1c601c6798e58
J Cole Morrison <j.cole.morrison@gmail.com>
2018-02-04 12:59:44 -0800

    clean up validate

--- src/validate/index.js
+++ src/validate/index.js
@@ -28,126 +28,6 @@ const jshintOpts = {
   node: true,
 }
 
-async function validateOld (env, opts) {
-  let aws
-  let templateFiles
-  let errors
-  let name = env
-  const profile = opts && opts.profile
-
-
-  if (!name) {
-    try {
-      name = await inquireTemplateName('validate')
-    } catch (error) {
-      return log.e(error.message)
-    }
-  }
-
-  try {
-    // searches for the default one if none is passed
-    aws = configAWS(_getProfile(profile || 'default'))
-    templateFiles = getTemplateFiles(name)
-    errors = []
-
-    // validate each file individually for better error reports
-    templateFiles.forEach((sf) => {
-      const isDir = fs.lstatSync(sf).isDirectory()
-      const cfnProp = getCfnPropType(sf)
-
-      if (!isDir) {
-        try {
-          const j = fs.readFileSync(path.resolve(sf), 'utf8')
-
-          jshint(j, jshintOpts)
-
-          if (jshint.errors.length > 0) errors.push({ file: sf, errors: jshint.errors })
-        } catch (error) {
-          throw error
-        }
-      } else {
-        if (cfnProp === 'Description') throw new Error(DESCRIPTION_ERROR_MSG)
-
-        glob.sync(`${sf}/**/*.+(js|json)`).forEach((f) => {
-          try {
-            const j = fs.readFileSync(path.resolve(f), 'utf8')
-
-            jshint(j, jshintOpts)
-
-            if (jshint.errors.length > 0) errors.push({ file: f, errors: jshint.errors })
-          } catch (error) {
-            throw error
-          }
-        })
-      }
-    })
-  } catch (error) {
-    return log.e(error.message)
-  }
-
-  let ecount = 0
-
-  errors.forEach((e) => {
-    log.p(chk.underline.whiteBright(`\nErrors found in ./src${e.file.split('src')[1]}\n`))
-
-    e.errors.forEach((e) => {
-      log.p(`  Line ${e.line} - ${e.reason}`)
-    })
-
-    log.p()
-
-    ecount += e.errors.length
-  })
-
-  if (errors.length > 0) {
-    return log.e(chk.red(`${ecount} syntax errors found across ${errors.length} files.  Please fix before continuing.\n`))
-  }
-
-  log.p()
-  log.s(`No syntax errors found across ${templateFiles.length} files for ${chk.cyan(name)} template!\n`)
-  log.i('Beginning Cloudformation Template Validation...\n')
-
-  // DONE: if at this point, we then everything is good to go
-  // we need to..
-  // a) check if src/ even has any damn folders
-  // b) check if they've passed a valid name of a template and look for it
-  // c) if they didn't pass anything, grab all the dirs, and show them as options
-  // d) when either (b) or (c) succeed, begin looping through every json file
-  // e) if any file fails, break out of the loop, and show them the failure
-
-  let templateObject
-
-  try {
-    templateObject = getTemplateAsObject(name)
-  } catch (error) {
-    return log.e(error.message)
-  }
-
-  const cfn = new aws.CloudFormation()
-
-  return cfn.validateTemplate({ TemplateBody: JSON.stringify(templateObject) }, (err) => {
-    if (err) {
-      log.e(`CloudFormation Validation error for template ${chk.cyan(name)}:\n`)
-      return log.m(`${err.message}\n`)
-    }
-
-    log.s('No CloudFormation Template Errors found!\n')
-    return log.i('Reminder - the validate-template API will not catch every error.  Some can only be found by deploying the full template.\n')
-  })
-
-  // DONE: after that we'll need to loop through and validate each template..
-  // a) create a .tmp/ folder
-  // b) call the build function down on just that one template (Which highlights the need for an individual build function)
-  // c) put the build template in .tmp/
-  // d) call the SDK validate template function on the template.
-  // e) either return the errors, or return a success message!
-  // IMPORTANT: I need to tell them that the damn user they're using needs access to that function of cloudformation
-
-  // DONE: we just need to bring in inquirer JS and, if they don't supply a template
-  // let them choose one and execute based on that.  This is eligible for helper function since
-  // it's going to be used in Deploy and Update as well.
-}
-
 module.exports = async function validate (env, opts) {
   log.p()
   let profile = opts && opts.profile
@@ -203,7 +83,7 @@ module.exports = async function validate (env, opts) {
   }
 
   log.s(`No syntax errors found across ${templateFiles.length} files for ${chk.cyan(name)} template!`)
-  log.i('Beginning Cloudformation Template Validation...', 0)
+  log.i('Beginning Cloudformation Template Validation...')
 
   // Run Template through CloudFormation
   const templateObject = getTemplateAsObject(name)
@@ -213,5 +93,5 @@ module.exports = async function validate (env, opts) {
   await cfn.validateTemplate({ TemplateBody: JSON.stringify(templateObject) }).promise()
 
   log.s('No CloudFormation Template Errors found!')
-  return log.i('Reminder - the validate-template API will not catch every error.  Some can only be found by deploying the full template.', 3)
+  return log.i('Reminder - the validate-template API will not catch every error.  Some can only be found by deploying the full template.', 2)
 }
