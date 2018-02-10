d8ca8ee18eabfdf5e0f302dd1308b58b35529e03
J Cole Morrison <j.cole.morrison@gmail.com>
2018-02-10 12:12:21 -0800

    don't try catch unnecessarily due to catch all handler

--- src/utils/stacks.js
+++ src/utils/stacks.js
@@ -4,196 +4,172 @@ const chk = require('chalk')
 const { log } = require('./index')
 
 exports.addTag = async () => {
-  try {
-    const tagName = await inq.prompt({
-      type: 'input',
-      message: `Tag name ${chk.gray('(leave blank when done)')}:`,
-      name: 'value',
-    })
+  const tagName = await inq.prompt({
+    type: 'input',
+    message: `Tag name ${chk.gray('(leave blank when done)')}:`,
+    name: 'value',
+  })
 
-    if (!tagName.value) return false
+  if (!tagName.value) return false
 
-    const tagValue = await inq.prompt({
-      type: 'input',
-      message: `Tag value for ${chk.cyan(tagName.value)}:`,
-      name: 'value',
-    })
+  const tagValue = await inq.prompt({
+    type: 'input',
+    message: `Tag value for ${chk.cyan(tagName.value)}:`,
+    name: 'value',
+  })
 
-    return { Key: tagName.value, Value: tagValue.value }
-  } catch (error) {
-    throw error
-  }
+  return { Key: tagName.value, Value: tagValue.value }
 }
 
 exports.addTags = async (tags = [], done) => {
   if (!done) {
-    try {
-      const tag = await exports.addTag()
+    const tag = await exports.addTag()
 
-      if (tag) {
-        tags.push(tag)
+    if (tag) {
+      tags.push(tag)
 
-        await exports.addTags(tags, false)
-      }
-    } catch (error) {
-      throw error
+      await exports.addTags(tags, false)
     }
   }
   return tags
 }
 
 exports.addIamRole = async (defaultVal) => {
-  try {
-    const role = await inq.prompt({
-      type: 'input',
-      message: 'ARN of IAM Role to use:',
-      name: 'arn',
-      default: defaultVal,
-    })
+  const role = await inq.prompt({
+    type: 'input',
+    message: 'ARN of IAM Role to use:',
+    name: 'arn',
+    default: defaultVal,
+  })
 
-    return role.arn ? role.arn : false
-  } catch (error) {
-    throw error
-  }
+  return role.arn ? role.arn : false
 }
 
 exports.createSNSTopic = async (region, aws) => {
-  try {
-    const topic = await inq.prompt([
-      {
-        type: 'input',
-        message: 'Name of SNS Topic:',
-        name: 'name',
-        validate: i => (i ? true : 'name required!'),
-      },
-      {
-        type: 'list',
-        message: 'Type of Subscription for the topic:',
-        choices: [
-          'http',
-          'https',
-          'email',
-          'email-json',
-          'sms',
-          'sqs',
-          'application',
-          'lambda',
-        ],
-        name: 'protocol',
-      },
-      {
-        type: 'input',
-        message: 'Endpoint for the subscription:',
-        name: 'endpoint', // not required, since Subscribe doesn't require it.
-      },
-    ])
+  const topic = await inq.prompt([
+    {
+      type: 'input',
+      message: 'Name of SNS Topic:',
+      name: 'name',
+      validate: i => (i ? true : 'name required!'),
+    },
+    {
+      type: 'list',
+      message: 'Type of Subscription for the topic:',
+      choices: [
+        'http',
+        'https',
+        'email',
+        'email-json',
+        'sms',
+        'sqs',
+        'application',
+        'lambda',
+      ],
+      name: 'protocol',
+    },
+    {
+      type: 'input',
+      message: 'Endpoint for the subscription:',
+      name: 'endpoint', // not required, since Subscribe doesn't require it.
+    },
+  ])
 
-    const sns = new aws.SNS({ region })
-    log.i('creating SNS Topic...')
-    const { TopicArn } = await sns.createTopic({ Name: topic.name }).promise()
-    await sns.subscribe({ TopicArn, Protocol: topic.protocol, Endpoint: topic.endpoint }).promise()
+  const sns = new aws.SNS({ region })
+  log.i('creating SNS Topic...')
+  const { TopicArn } = await sns.createTopic({ Name: topic.name }).promise()
+  await sns.subscribe({ TopicArn, Protocol: topic.protocol, Endpoint: topic.endpoint }).promise()
 
-    return { TopicArn }
-  } catch (error) {
-    throw error
-  }
+  return { TopicArn }
 }
 
 exports.setStackSNS = async (region, aws, prev) => {
-  try {
-    let sns = await inq.prompt({
-      type: 'list',
-      message: 'How would you like to set up SNS for the stack?',
-      default: 'existing',
-      name: 'type',
-      choices: [
-        { name: 'Use existing SNS topic ARN', value: 'existing' },
-        { name: 'Create new SNS topic', value: 'new' },
-      ],
+  let sns = await inq.prompt({
+    type: 'list',
+    message: 'How would you like to set up SNS for the stack?',
+    default: 'existing',
+    name: 'type',
+    choices: [
+      { name: 'Use existing SNS topic ARN', value: 'existing' },
+      { name: 'Create new SNS topic', value: 'new' },
+    ],
+  })
+
+  if (sns.type === 'new') {
+    sns = await exports.createSNSTopic(region, aws)
+  } else {
+    sns = await inq.prompt({
+      type: 'input',
+      message: 'ARN of SNS topic:',
+      name: 'TopicArn',
+      default: prev,
     })
-
-    if (sns.type === 'new') {
-      sns = await exports.createSNSTopic(region, aws)
-    } else {
-      sns = await inq.prompt({
-        type: 'input',
-        message: 'ARN of SNS topic:',
-        name: 'TopicArn',
-        default: prev,
-      })
-    }
-
-    return sns.TopicArn
-  } catch (error) {
-    throw error
   }
+
+  return sns.TopicArn
 }
 
 exports.selectAdvancedStackOptions = async (region, aws, prevOpts = {}, isUpdate) => {
   const options = {}
 
-  try {
-    let sns = await inq.prompt({
-      type: 'confirm',
-      message: 'Set up an SNS notifications for this stack?',
-      default: false,
-      name: 'add',
-    })
+  let sns = await inq.prompt({
+    type: 'confirm',
+    message: 'Set up an SNS notifications for this stack?',
+    default: false,
+    name: 'add',
+  })
 
-    if (sns) {
-      sns = await exports.setStackSNS(region, aws, prevOpts.snsTopicArn)
+  if (sns) {
+    sns = await exports.setStackSNS(region, aws, prevOpts.snsTopicArn)
 
-      if (sns) options.snsTopicArn = sns
-    }
-
-    if (isUpdate) {
-      const { terminationProtection, timeout, onFailure } = prevOpts
-
-      options.terminationProtection = terminationProtection
-      options.timeout = timeout
-      options.onFailure = onFailure
-
-      return options
-    }
-
-    const termination = await inq.prompt({
-      type: 'confirm',
-      message: 'Enable termination protection?',
-      default: false,
-      name: 'enable',
-    })
-
-    if (termination.enable) options.terminationProtection = true
-
-    const timeout = await inq.prompt({
-      type: 'input',
-      message: `Set timeout in minutes before stack creation fails ${chk.gray('leave blank for none')}:`,
-      name: 'minutes',
-      validate: (i) => {
-        if (i) {
-          if (!/^([+-]?[1-9]\d*|0)$/.test(i)) return 'Value must be an integer'
-        }
-        return true
-      },
-    })
-
-    if (timeout.minutes) options.timeout = parseInt(timeout.minutes, 10)
-
-    const onFailure = await inq.prompt({
-      type: 'list',
-      message: 'Select an action to take on stack creation failure:',
-      choices: [
-        { name: 'nothing', value: 'DO_NOTHING' },
-        { name: 'rollback', value: 'ROLLBACK' },
-        { name: 'delete', value: 'DELETE' },
-      ],
-      name: 'action',
-    })
-
-    options.onFailure = onFailure.action
-  } catch (error) {
-    throw error
+    if (sns) options.snsTopicArn = sns
   }
+
+  if (isUpdate) {
+    const { terminationProtection, timeout, onFailure } = prevOpts
+
+    options.terminationProtection = terminationProtection
+    options.timeout = timeout
+    options.onFailure = onFailure
+
+    return options
+  }
+
+  const termination = await inq.prompt({
+    type: 'confirm',
+    message: 'Enable termination protection?',
+    default: false,
+    name: 'enable',
+  })
+
+  if (termination.enable) options.terminationProtection = true
+
+  const timeout = await inq.prompt({
+    type: 'input',
+    message: `Set timeout in minutes before stack creation fails ${chk.gray('leave blank for none')}:`,
+    name: 'minutes',
+    validate: (i) => {
+      if (i) {
+        if (!/^([+-]?[1-9]\d*|0)$/.test(i)) return 'Value must be an integer'
+      }
+      return true
+    },
+  })
+
+  if (timeout.minutes) options.timeout = parseInt(timeout.minutes, 10)
+
+  const onFailure = await inq.prompt({
+    type: 'list',
+    message: 'Select an action to take on stack creation failure:',
+    choices: [
+      { name: 'nothing', value: 'DO_NOTHING' },
+      { name: 'rollback', value: 'ROLLBACK' },
+      { name: 'delete', value: 'DELETE' },
+    ],
+    name: 'action',
+  })
+
+  options.onFailure = onFailure.action
 
   return options
 }
@@ -535,13 +511,9 @@ exports.createStack = async (template, name, stack, aws) => {
     }
   }
 
-  try {
-    const { StackId } = await cfn.createStack(opts).promise()
+  const { StackId } = await cfn.createStack(opts).promise()
 
-    return StackId
-  } catch (error) {
-    throw error
-  }
+  return StackId
 }
 
 exports.updateStack = async (template, name, stack, aws) => {
