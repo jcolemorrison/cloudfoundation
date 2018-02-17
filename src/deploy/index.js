10e8d2932b34cd005b32869dc16b2ab63c6cc9e6
J Cole Morrison <j.cole.morrison@gmail.com>
2018-02-17 11:43:23 -0800

    first pass at deploy tests

--- src/deploy/index.js
+++ src/deploy/index.js
@@ -9,13 +9,64 @@ const awsUtils = require('../utils/aws.js')
 
 const { cyan } = chk
 
-module.exports = async function deploy (env, opts = {}) {
+exports.inqStackName = async (templateName) => {
+  const stack = await inq.prompt({
+    type: 'input',
+    name: 'name',
+    message: `What do you want to name the stack being deployed from template ${chk.cyan(templateName)}?`,
+    validate (input) {
+      if (!input) return 'A name for the stack is required!'
+      if (!/^[a-zA-Z0-9-]+$/.test(input)) return 'Stack names can only contain alphanumeric characters and hyphens'
+      if (input.length > 128) return 'Stack names can only be 128 characters long'
+      return true
+    },
+  })
+
+  return stack.name
+}
+
+exports.useExistingDeploy = async (stack, stackName, templateName) => {
+  if (stack.stackId) {
+    throw new Error(chk.red(`Stack ${cyan(stackName)} already exists.  Run ${cyan(`cfdn update ${templateName} --stackname ${stackName}`)} to modify it.`))
+  }
+
+  const msg = `Stack with name ${chk.cyan(stackName)} found for template ${chk.cyan(templateName)}`
+
+  return stackUtils.confirmStack(templateName, stackName, stack, msg, 'Deploy')
+}
+
+exports.createStackSettings = async (profile, templateParams, aws) => {
+  const region = await utils.selectRegion(profile, 'Which region would you like to deploy this stack to?')
+
+  const params = await paramUtils.selectStackParams(templateParams, region, aws)
+
+  const options = await stackUtils.selectStackOptions(region, aws)
+
+  return {
+    profile: profile.name,
+    region,
+    options,
+    parameters: params,
+  }
+}
+
+exports.createSaveSettings = (rc, templateName, stackName, stack) => {
+  const saveSettings = { ...rc }
+
+  saveSettings.templates[templateName] = {
+    ...saveSettings.templates[templateName],
+    [stackName]: { ...stack },
+  }
+
+  return saveSettings
+}
+
+exports.deploy = async function deploy (env, opts = {}) {
   const cwd = process.cwd()
   let templateName = env
   let profile = opts.profile
   let stackName = opts.stackname
   let stack
-  let region
   let saveSettings
   let useExisting
 
@@ -25,37 +76,19 @@ module.exports = async function deploy (env, opts = {}) {
   const rc = fs.readJsonSync(`${cwd}/.cfdnrc`)
   rc.templates = rc.templates || {}
 
-  if (!stackName) {
-    const stackInq = await inq.prompt({
-      type: 'input',
-      name: 'name',
-      message: `What do you want to name the stack being deployed from template ${chk.cyan(templateName)}?`,
-      validate (input) {
-        if (!input) return 'A name for the stack is required!'
-        if (!/^[a-zA-Z0-9-]+$/.test(input)) return 'Stack names can only contain alphanumeric characters and hyphens'
-        if (input.length > 128) return 'Stack names can only be 128 characters long'
-        return true
-      },
-    })
-
-    if (stackInq && stackInq.name) stackName = stackInq.name
-  }
+  if (!stackName) stackName = await exports.inqStackName(templateName)
 
   stack = rc.templates[templateName] && rc.templates[templateName][stackName]
 
   // Load predefined stack settings if available
   if (stack) {
-    if (stack.stackId) {
-      throw new Error(chk.red(`Stack ${cyan(stackName)} already exists.  Run ${cyan(`cfdn update ${templateName} --stackname ${stackName}`)} to modify it.`))
-    }
-
-    const msg = `Stack with name ${chk.cyan(stackName)} found for template ${chk.cyan(templateName)}`
-    useExisting = await stackUtils.confirmStack(templateName, stackName, stack, msg, 'Deploy')
+    useExisting = await this.exports.useExistingDeploy(stack, stackName, templateName)
 
     if (useExisting) profile = stack.profile
     else return utils.log.e(`Stack ${cyan(stackName)} already exists.  Either use the settings you have configured, choose a different stackname, or delete the stack from your ${chk.cyan('.stacks')} file.`)
   }
 
+  // Get profile for AWS usage
   if (!profile) profile = await profileUtils.selectFromAllProfiles('Which profile would you like to use to deploy this stack?')
   else profile = profileUtils.getFromAllProfiles(profile)
 
@@ -63,39 +96,23 @@ module.exports = async function deploy (env, opts = {}) {
   const template = utils.getTemplateAsObject(templateName)
 
   // Create stack settings if unavailable
-  if (!stack) {
-    region = await utils.selectRegion(profile, 'Which region would you like to deploy this stack to?')
+  if (!stack) stack = await exports.createStackSettings(profile, template.Parameters, aws)
 
-    const params = await paramUtils.selectStackParams(template.Parameters, region, aws)
-
-    const options = await stackUtils.selectStackOptions(region, aws)
-
-    stack = {
-      profile: profile.name,
-      region,
-      options,
-      parameters: params,
-    }
-  }
-
+  // Go through param set up if not using pre-defined settings.
   if (!useExisting) {
     if (!await stackUtils.confirmStack(templateName, stackName, stack, false, 'Deploy')) return false
 
     saveSettings = await inq.prompt({
       type: 'confirm',
-      name: 'yes',
+      name: 'use',
       message: 'Would you like to save these options for later deploys and updates?',
       default: true,
     })
   }
 
-  // Write the stack file first, in case something fails, user won't have to re-input
-  if (useExisting || saveSettings) {
-    saveSettings = { ...rc }
-    saveSettings.templates[templateName] = {
-      ...saveSettings.templates[templateName],
-      [stackName]: { ...stack },
-    }
+  // Write the updated RC file first, in case something fails, user won't have to re-input
+  if (useExisting || saveSettings.use) {
+    saveSettings = exports.createSaveSettings(rc, templateName, stackName, stack)
     utils.writeRcFile(cwd, saveSettings)
   }
 
--- test/deploy/index.test.js
+++ test/deploy/index.test.js
@@ -9,115 +9,261 @@ const stackUtils = require('../../src/utils/stacks')
 const profileUtils = require('../../src/profiles/utils')
 const awsUtils = require('../../src/utils/aws.js')
 
-const deploy = require('../../src/deploy')
+const cmd = require('../../src/deploy')
+
 const cwd = process.cwd()
 
-describe('Deploy Function', () => {
-  it('should deploy the stack and return a success and info log', () => {
-    const template = {
-      Parameters: {
-        ParamOne: { Type: 'String' },
-        ParamTwo: { Type: 'String' },
-      },
+describe('Deploy Functions', () => {
+  let log
+
+  beforeEach(() => {
+    log = {
+      p: sinon.stub(utils.log, 'p'),
+      e: sinon.stub(utils.log, 'e'),
+      s: sinon.stub(utils.log, 's'),
+      i: sinon.stub(utils.log, 'i'),
+      m: sinon.stub(utils.log, 'm'),
     }
-    const rc = {
-      project: 'test',
-      profiles: {
-        testProfile: {
-          aws_access_key_id: 'abcd',
-          aws_secret_access_key: 'efgh',
-          region: 'us-east-1',
-        },
-      },
-    }
-    const stack = {
-      profile: 'testProfile',
-      region: 'us-east-1',
-      options: {
-        tags: [
-          {
-            Key: 'update',
-            Value: 'test',
+  })
+
+  afterEach(() => {
+    log.p.restore()
+    log.e.restore()
+    log.s.restore()
+    log.i.restore()
+    log.m.restore()
+  })
+
+  describe('#deploy', () => {
+    let readJsonSync
+    let inquireTemplateName
+    let inqStackName
+    let selectFromAllProfiles
+    let configAWS
+    let getTemplateAsObject
+    let createStackSettings
+    let confirmStack
+    let inqPrompt
+    let createSaveSettings
+    let createStack
+    let writeRcFile
+
+    beforeEach(() => {
+      readJsonSync = sinon.stub(fs, 'readJsonSync')
+      inquireTemplateName = sinon.stub(utils, 'inquireTemplateName')
+      inqStackName = sinon.stub(cmd, 'inqStackName')
+      selectFromAllProfiles = sinon.stub(profileUtils, 'selectFromAllProfiles')
+      configAWS = sinon.stub(awsUtils, 'configAWS')
+      getTemplateAsObject = sinon.stub(utils, 'getTemplateAsObject')
+      createStackSettings = sinon.stub(cmd, 'createStackSettings')
+      confirmStack = sinon.stub(stackUtils, 'confirmStack')
+      inqPrompt = sinon.stub(inq, 'prompt')
+      createSaveSettings = sinon.stub(cmd, 'createSaveSettings')
+      createStack = sinon.stub(stackUtils, 'createStack')
+      writeRcFile = sinon.stub(utils, 'writeRcFile')
+    })
+
+    afterEach(() => {
+      readJsonSync.restore()
+      inquireTemplateName.restore()
+      inqStackName.restore()
+      selectFromAllProfiles.restore()
+      configAWS.restore()
+      getTemplateAsObject.restore()
+      createStackSettings.restore()
+      confirmStack.restore()
+      inqPrompt.restore()
+      createSaveSettings.restore()
+      createStack.restore()
+      writeRcFile.restore()
+    })
+
+    it('should deploy the stack and return an info message on success with no predefined stack, saving settings, or cli options', () => {
+      const testTemplate = { Parameters: { ParamOne: 'Test', ParamTwo: 'Test' } }
+
+      readJsonSync.returns({})
+      inquireTemplateName.returns('testTemplateName')
+      inqStackName.returns('testStackName')
+      selectFromAllProfiles.returns('selectedProfile')
+      configAWS.returns('configuredAWS')
+      getTemplateAsObject.returns(testTemplate)
+      createStackSettings.returns({ test: 'stack' })
+      confirmStack.returns(true)
+      inqPrompt.returns({ use: true })
+      createSaveSettings.returns({
+        templates: {
+          testTemplateName: {
+            testStackName: {
+              test: 'stack',
+            },
           },
-          {
-            Key: 'updated',
-            Value: 'tested',
-          },
-        ],
-        advanced: {
-          snsTopicArn: 'arn:aws:sns:us-east-1:1234567890:testsns',
-          terminationProtection: true,
-          timeout: 100,
-          onFailure: 'ROLLBACK',
         },
-        capabilityIam: true,
-      },
-      parameters: {
-        paramOne: 'test',
-        paramTwo: 'test',
-      },
-    }
-
-    const fetchedProfile = {
-      aws_access_key_id: 'abcd',
-      aws_secret_access_key: 'efgh',
-      region: 'us-east-1',
-      name: 'testProfile',
-    }
-
-    const saveSettings = {
-      ...rc,
-      templates: {
-        test: {
-          teststack: stack,
-        },
-      },
-    }
-    return deploy(env, opts).then(() => {
-
-      expect(configAWS.getCall(0).args[0]).to.deep.equal(fetchedProfile)
-      expect(getTemplateAsObject.getCall(0).args[0]).to.equal('test')
-      expect(selectRegion.getCall(0).args).to.deep.equal([
-        fetchedProfile,
-        'Which region would you like to deploy this stack to?',
-      ])
-      expect(selectStackParams.getCall(0).args).to.deep.equal([
-        template.Parameters,
-        'us-east-1',
-        'configuredAws',
-      ])
-      expect(selectStackOptions.getCall(0).args).to.deep.equal([
-        'us-east-1',
-        'configuredAws',
-      ])
-      expect(inqPrompt.lastCall.args[0]).to.deep.equal({
-        type: 'confirm',
-        name: 'yes',
-        message: 'Would you like to save these options for later deploys and updates?',
-        default: true,
       })
-      expect(writeRcFile.firstCall.args[0]).to.deep.equal([
-        cwd,
-        saveSettings,
-      ])
+      createStack.returns('test:stack:id')
 
-      expect(createStack.getCall(0).args).to.deep.equal([
-        template,
-        'teststack',
-        stack,
-        'configuredAws',
-      ])
+      return cmd.deploy().then(() => {
+        expect(inqStackName.lastCall.args[0]).to.equal('testTemplateName')
+        expect(configAWS.lastCall.args[0]).to.deep.equal('selectedProfile')
+        expect(getTemplateAsObject.lastCall.args[0]).to.equal('testTemplateName')
+        expect(createStackSettings.lastCall.args).to.deep.equal([
+          'selectedProfile',
+          testTemplate.Parameters,
+          'configuredAWS',
+        ])
+        expect(confirmStack.lastCall.args).to.deep.equal([
+          'testTemplateName',
+          'testStackName',
+          { test: 'stack' },
+          false,
+          'Deploy',
+        ])
+        // Whether ot save settings
+        expect(inqPrompt.lastCall.args[0]).to.deep.equal({
+          type: 'confirm',
+          name: 'use',
+          message: 'Would you like to save these options for later deploys and updates?',
+          default: true,
+        })
+        expect(createSaveSettings.firstCall.args).to.deep.equal([
+          { templates: {} },
+          'testTemplateName',
+          'testStackName',
+          { test: 'stack' },
+        ])
 
-      saveSettings.templates.test.teststack.stackId = 'test:stack:id'
+        expect(createStack.getCall(0).args).to.deep.equal([
+          testTemplate,
+          'testStackName',
+          { test: 'stack' },
+          'configuredAWS',
+        ])
 
-      expect(writeRcFile.lastCall.args[0]).to.deep.equal([
-        cwd,
-        saveSettings,
-      ])
-      expect(log.i.lastCall.args[0]).to.deep.equal([
-        `StackId: ${chk.cyan('test:stack:id')}`,
-        3,
-      ])
+        expect(writeRcFile.lastCall.args).to.deep.equal([
+          cwd,
+          {
+            templates: {
+              testTemplateName: {
+                testStackName: {
+                  test: 'stack',
+                  stackId: 'test:stack:id',
+                },
+              },
+            },
+          },
+        ])
+
+        expect(log.i.lastCall.args).to.deep.equal([
+          `StackId: ${chk.cyan('test:stack:id')}`,
+          3,
+        ])
+      })
     })
   })
+  // it('should deploy the stack and return a success and info log', () => {
+  //   const template = {
+  //     Parameters: {
+  //       ParamOne: { Type: 'String' },
+  //       ParamTwo: { Type: 'String' },
+  //     },
+  //   }
+  //   const rc = {
+  //     project: 'test',
+  //     profiles: {
+  //       testProfile: {
+  //         aws_access_key_id: 'abcd',
+  //         aws_secret_access_key: 'efgh',
+  //         region: 'us-east-1',
+  //       },
+  //     },
+  //   }
+  //   const stack = {
+  //     profile: 'testProfile',
+  //     region: 'us-east-1',
+  //     options: {
+  //       tags: [
+  //         {
+  //           Key: 'update',
+  //           Value: 'test',
+  //         },
+  //         {
+  //           Key: 'updated',
+  //           Value: 'tested',
+  //         },
+  //       ],
+  //       advanced: {
+  //         snsTopicArn: 'arn:aws:sns:us-east-1:1234567890:testsns',
+  //         terminationProtection: true,
+  //         timeout: 100,
+  //         onFailure: 'ROLLBACK',
+  //       },
+  //       capabilityIam: true,
+  //     },
+  //     parameters: {
+  //       paramOne: 'test',
+  //       paramTwo: 'test',
+  //     },
+  //   }
+
+  //   const fetchedProfile = {
+  //     aws_access_key_id: 'abcd',
+  //     aws_secret_access_key: 'efgh',
+  //     region: 'us-east-1',
+  //     name: 'testProfile',
+  //   }
+
+  //   const saveSettings = {
+  //     ...rc,
+  //     templates: {
+  //       test: {
+  //         teststack: stack,
+  //       },
+  //     },
+  //   }
+  //   return deploy(env, opts).then(() => {
+
+  //     expect(configAWS.getCall(0).args[0]).to.deep.equal(fetchedProfile)
+  //     expect(getTemplateAsObject.getCall(0).args[0]).to.equal('test')
+  //     expect(selectRegion.getCall(0).args).to.deep.equal([
+  //       fetchedProfile,
+  //       'Which region would you like to deploy this stack to?',
+  //     ])
+  //     expect(selectStackParams.getCall(0).args).to.deep.equal([
+  //       template.Parameters,
+  //       'us-east-1',
+  //       'configuredAws',
+  //     ])
+  //     expect(selectStackOptions.getCall(0).args).to.deep.equal([
+  //       'us-east-1',
+  //       'configuredAws',
+  //     ])
+  //     expect(inqPrompt.lastCall.args[0]).to.deep.equal({
+  //       type: 'confirm',
+  //       name: 'yes',
+  //       message: 'Would you like to save these options for later deploys and updates?',
+  //       default: true,
+  //     })
+  //     expect(writeRcFile.firstCall.args[0]).to.deep.equal([
+  //       cwd,
+  //       saveSettings,
+  //     ])
+
+  //     expect(createStack.getCall(0).args).to.deep.equal([
+  //       template,
+  //       'teststack',
+  //       stack,
+  //       'configuredAws',
+  //     ])
+
+  //     saveSettings.templates.test.teststack.stackId = 'test:stack:id'
+
+  //     expect(writeRcFile.lastCall.args[0]).to.deep.equal([
+  //       cwd,
+  //       saveSettings,
+  //     ])
+  //     expect(log.i.lastCall.args[0]).to.deep.equal([
+  //       `StackId: ${chk.cyan('test:stack:id')}`,
+  //       3,
+  //     ])
+  //   })
+  // })
 })
