e1d504c86341b23e03efcba512a2c9a1fd2b1f40
J Cole Morrison <j.cole.morrison@gmail.com>
2018-02-27 21:48:35 -0800

    profile remove function tests

--- src/profiles/remove.js
+++ src/profiles/remove.js
@@ -1,27 +1,21 @@
 const inq = require('inquirer')
 const chk = require('chalk')
-const { log } = require('../utils')
+const utils = require('../utils')
+const profileUtils = require('./utils')
 
-const {
-  getScopedProfiles,
-  selectProfile,
-  writeGlobalProfiles,
-  writeLocalProfiles,
-} = require('./utils')
-
-module.exports = async function removeProfile (env, opts) {
-  log.p()
-  const { global, local } = opts || {}
+module.exports = async function removeProfile (env, opts = {}) {
+  utils.log.p()
+  const { global, local } = opts
   let name = env
   let profile
 
-  const { profiles, scope } = await getScopedProfiles(global, local, 'remove')
+  const { profiles, scope } = await profileUtils.getScopedProfiles(global, local, 'remove')
 
   if (name) {
     if (!Object.keys(profiles).includes(name)) throw new Error(`Profile ${name} does not exist!`)
     profile = { ...profiles[name], name }
   } else {
-    profile = await selectProfile(profiles, 'Which profile would you like to remove?')
+    profile = await profileUtils.selectProfile(profiles, 'Which profile would you like to remove?')
     name = profile.name
   }
 
@@ -40,8 +34,8 @@ module.exports = async function removeProfile (env, opts) {
 
   delete profiles[name]
 
-  if (scope === 'local') writeLocalProfiles(profiles)
-  else writeGlobalProfiles(profiles)
+  if (scope === 'local') profileUtils.writeLocalProfiles(profiles)
+  else profileUtils.writeGlobalProfiles(profiles)
 
-  return log.s(`Profile ${chk.cyan(name)} successfully removed!`, 2)
+  return utils.log.s(`Profile ${chk.cyan(name)} successfully removed!`, 2)
 }
---
+++ test/profiles/remove.test.js
@@ -0,0 +1,144 @@
+const expect = require('chai').expect
+const sinon = require('sinon')
+const inq = require('inquirer')
+const chk = require('chalk')
+const utils = require('../../src/utils')
+const profileUtils = require('../../src/profiles/utils.js')
+
+const cmd = require('../../src/profiles/remove.js')
+
+describe('Profile Remove Function', () => {
+  let log
+  let getScopedProfiles
+  let inqPrompt
+  let selectProfile
+  let writeLocalProfiles
+  let writeGlobalProfiles
+
+
+  beforeEach(() => {
+    log = {
+      p: sinon.stub(utils.log, 'p'),
+      e: sinon.stub(utils.log, 'e'),
+      s: sinon.stub(utils.log, 's'),
+      i: sinon.stub(utils.log, 'i'),
+      m: sinon.stub(utils.log, 'm'),
+    }
+
+    getScopedProfiles = sinon.stub(profileUtils, 'getScopedProfiles')
+    inqPrompt = sinon.stub(inq, 'prompt')
+    selectProfile = sinon.stub(profileUtils, 'selectProfile')
+    writeLocalProfiles = sinon.stub(profileUtils, 'writeLocalProfiles')
+    writeGlobalProfiles = sinon.stub(profileUtils, 'writeGlobalProfiles')
+  })
+
+  afterEach(() => {
+    log.p.restore()
+    log.e.restore()
+    log.s.restore()
+    log.i.restore()
+    log.m.restore()
+
+    getScopedProfiles.restore()
+    inqPrompt.restore()
+    selectProfile.restore()
+    writeLocalProfiles.restore()
+    writeGlobalProfiles.restore()
+  })
+
+  it('should write the global profiles without the removed profile via selection', () => {
+    getScopedProfiles.returns({
+      profiles: {
+        oldProfileName: {},
+        profileName: {},
+      },
+      scope: 'global',
+    })
+    selectProfile.returns({ name: 'profileName' })
+    inqPrompt.returns({ remove: true })
+
+    return cmd().then(() => {
+      expect(writeGlobalProfiles.lastCall.args[0]).to.deep.equal({
+        oldProfileName: {},
+      })
+      expect(log.s.lastCall.args).to.deep.equal([
+        `Profile ${chk.cyan('profileName')} successfully removed!`,
+        2,
+      ])
+    })
+  })
+
+  it('should write local profiles without the removed profile via selection', () => {
+    getScopedProfiles.returns({
+      profiles: {
+        oldProfileName: {},
+        profileName: {},
+      },
+      scope: 'local',
+    })
+    selectProfile.returns({ name: 'profileName' })
+    inqPrompt.returns({ remove: true })
+
+    return cmd().then(() => {
+      expect(writeLocalProfiles.lastCall.args[0]).to.deep.equal({
+        oldProfileName: {},
+      })
+      expect(log.s.lastCall.args).to.deep.equal([
+        `Profile ${chk.cyan('profileName')} successfully removed!`,
+        2,
+      ])
+    })
+  })
+
+  it('should return false if they do not confirm', () => {
+    getScopedProfiles.returns({
+      profiles: {
+        oldProfileName: {},
+        profileName: {},
+      },
+      scope: 'local',
+    })
+    selectProfile.returns({ name: 'profileName' })
+    inqPrompt.returns({ remove: false })
+
+    return cmd().then((d) => {
+      expect(d).to.be.false
+    })
+  })
+
+  it('should write the global profiles if name passed via options', () => {
+    getScopedProfiles.returns({
+      profiles: {
+        oldProfileName: {},
+        profileName: {},
+      },
+      scope: 'global',
+    })
+    inqPrompt.returns({ remove: true })
+
+    return cmd('profileName').then(() => {
+      expect(writeGlobalProfiles.lastCall.args[0]).to.deep.equal({
+        oldProfileName: {},
+      })
+      expect(log.s.lastCall.args).to.deep.equal([
+        `Profile ${chk.cyan('profileName')} successfully removed!`,
+        2,
+      ])
+    })
+  })
+
+  it('should write the local profiles if name passed via options', () => {
+    getScopedProfiles.returns({
+      profiles: {
+        oldProfileName: {},
+        profileName: {},
+      },
+      scope: 'local',
+    })
+    inqPrompt.returns({ remove: true })
+
+    return cmd('missing').catch((e) => {
+      expect(e.message).to.equal('Profile missing does not exist!')
+    })
+  })
+})
